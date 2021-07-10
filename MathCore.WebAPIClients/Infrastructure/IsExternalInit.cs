e3a8000a1a23dc76c32eabeb0d467af50b46bd82
Infarh <shmachilin@yandex.ru>
2021-07-11 01:38:50 +0300

    Добавьте файлы проекта.

---
+++ MathCore.WebAPIClients/Infrastructure/IsExternalInit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// ReSharper disable once CheckNamespace
+namespace System.Runtime.CompilerServices
+{
+    internal class IsExternalInit
+    {
+    }
+}
---
+++ Tests/MathCore.WebAPIClients.ConsoleTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MathCore.WebAPIClients.ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
